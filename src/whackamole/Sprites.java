2715af5ab39dff1c6c688af4866be0d5969edd81
phillip paul hutchings <phillhutchings@hotmail.com>
2022-08-03 22:15:57 +0100

    update

---
+++ src/whackamole/Sprites.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package whackamole;
+
+/**
+ *
+ * @author hutch
+ */
+public interface Sprites {
+    
+    int BOARD_WIDTH = 800;
+    int BOARD_HEIGHT = 900;
+    int MOLE_WIDTH = 150;
+    int MOLE_HEIGHT = 150;
+    int MOLE_RAISE = 60;
+    
+}
